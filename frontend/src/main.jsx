80c7592f87e8dd520c6e38693fc4bceca8e0b4c9
Aditi Dharme <aditidharme77@gmail.com>
2026-09-02 15:34:51 +0530

    Fix Google OAuth provider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,7 @@
-
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { GoogleOAuthProvider } from "@react-oauth/google"
 
 import App from "./App"
 import Login from "./Login"
@@ -12,21 +12,23 @@ import "./index.css"
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
+    <GoogleOAuthProvider clientId="829903513386-b8si1jlqkiebufub8k5m560s5i3g8d74.apps.googleusercontent.com">
+      <BrowserRouter>
+        <Routes>
 
-        <Route path="/" element={<App />} />
+          <Route path="/" element={<App />} />
 
-        <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login />} />
 
-        <Route path="/signup" element={<Signup />} />
+          <Route path="/signup" element={<Signup />} />
 
-        <Route
-          path="/public/:token"
-          element={<PublicLink />}
-        />
+          <Route
+            path="/public/:token"
+            element={<PublicLink />}
+          />
 
-      </Routes>
-    </BrowserRouter>
+        </Routes>
+      </BrowserRouter>
+    </GoogleOAuthProvider>
   </StrictMode>
 )
